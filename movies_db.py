3f5db44142de5fed7f26eedbc2feb678156e986e
AditiJha26 <jhaaditi269@gmail.com>
2024-11-29 20:32:06 -0500

    Update movies_db.py

--- movies_db.py
+++ movies_db.py
@@ -1,10 +1,17 @@
 # Movies Database, Aditi Jha, November 29, 2024:
 
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the m.env file
+load_dotenv("m.env")
 
 # Database connection
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        database="movies"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
